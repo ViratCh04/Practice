e1f2c72af83aba5f8ffa0fbfccb392e4f6d8f665
ViratCh <viratchauhan2004.work@gmail.com>
2022-05-27 10:17:51 +0530

    for loop

--- automateboringal/flowcontrol/for.py
+++ automateboringal/flowcontrol/for.py
@@ -3,3 +3,15 @@ for num in range(201):
     total = total + num
 
 print(f"Sum of first 200 numbers is: {total}")
+
+print()
+print("Table of 3")
+j = 1
+for i in range(3, 31, 3):
+    print(f"3 x {j} = " + str(i))
+    j = j + 1
+
+print()
+print("number moonwalk")
+for k in range(10, -1, -2):
+    print(f"{k}")
